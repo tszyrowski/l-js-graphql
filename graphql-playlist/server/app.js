1e9cab70ad99447d425507a2d8c9ad27bfda5ce9
tomasz szyrowski <tszyrowski@gmail.com>
2021-09-11 15:36:40 +0100

    Working script for hard coded data and .env vars (#3)
     L1: Use graphqql with hardcoded data and mongo link with .env var

--- graphql-playlist/server/app.js
+++ graphql-playlist/server/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-// const { TextEncoder, TextDecoder } = require("util");
 
 const app = express();
 
 mongoose.connect(
-    'mongodb+srv://t-gql-ninja:<password>@gql-ninja.jdxic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    `mongodb+srv://t-gql-ninja:${
+        process.env.REACT_APP_MONGODB_PASS
+    }@gql-ninja.jdxic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 );
 mongoose.connection.once('open', () => {
     console.log("Connected to database");
